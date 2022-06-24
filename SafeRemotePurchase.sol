ee42d4104e6a07c891a23dff72da131d13d4dc90
timtimtim.eth <timothyshen1@hotmail.com>
2022-06-25 00:29:59 +0100

    Create SafeRemotePurchase.sol
    Added the SafeRemotePurchase for question 6

---
+++ SafeRemotePurchase.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract Purchase {
+    uint public value;
+    address payable public seller;
+    address payable public buyer;
+    uint256 public purchaseTime;
+
+    enum State { Created, Locked, Inactive }
+    // The state variable has a default value of the first member, `State.created`
+    State public state;
+
+    modifier condition(bool condition_) {
+        require(condition_);
+        _;
+    }
+
+
+    /// Only the seller can call this function.
+    error OnlySeller();
+    /// The function cannot be called at the current state.
+    error InvalidState();
+    /// The provided value has to be even.
+    error ValueNotEven();
+    /// The user has to be qualified to access the function
+    error NotQualify();
+
+
+    modifier onlySeller() {
+        if (msg.sender != seller)
+            revert OnlySeller();
+        _;
+    }
+
+    modifier inState(State state_) {
+        if (state != state_)
+            revert InvalidState();
+        _;
+    }
+
+    modifier checkQualification() {
+        if (msg.sender != buyer && block.timestamp < purchaseTime + 5 minutes)
+            revert NotQualify();
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+
+    // Ensure that `msg.value` is an even number.
+    // Division will truncate if it is an odd number.
+    // Check via multiplication that it wasn't an odd number.
+    constructor() payable {
+        seller = payable(msg.sender);
+        value = msg.value / 2;
+        if ((2 * value) != msg.value)
+            revert ValueNotEven();
+    }
+
+    /// Abort the purchase and reclaim the ether.
+    /// Can only be called by the seller before
+    /// the contract is locked.
+    function abort()
+    external
+    onlySeller
+    inState(State.Created)
+    {
+        emit Aborted();
+        state = State.Inactive;
+        // We use transfer here directly. It is
+        // reentrancy-safe, because it is the
+        // last call in this function and we
+        // already changed the state.
+        seller.transfer(address(this).balance);
+    }
+
+    /// Confirm the purchase as buyer.
+    /// Transaction has to include `2 * value` ether.
+    /// The ether will be locked until confirmReceived
+    /// is called.
+    function confirmPurchase()
+    external
+    inState(State.Created)
+    condition(msg.value == (2 * value))
+    payable
+    {
+        emit PurchaseConfirmed();
+        buyer = payable(msg.sender);
+        state = State.Locked;
+        // Set the time of the purchase.
+        purchaseTime = block.timestamp;
+    }
+
+    /// Confirm the purchase as seller.
+    /// The ether will be locked until confirmReceived
+    /// is called.
+    function completePurchase()
+    external
+    inState(State.Locked)
+    checkQualification
+    {
+        // Set the time of the purchase.
+        emit ItemReceived();
+        state = State.Inactive;
+        buyer.transfer(value);
+
+        emit SellerRefunded();
+        seller.transfer(3 * value);
+    }
+}
